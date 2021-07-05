540538109226763ad107c84246284dfc80abd70f
BootCampBL <satyabhandari43@gmail.com>
2021-07-05 11:36:07 +0530

    tring to push UC11

---
+++ IComputeEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Final_Solution
+{
+        public interface IComputeEmpWage
+        {
+            void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+            void computeEmpWage();
+
+            int getTotalWage(string company);
+        }
+}
